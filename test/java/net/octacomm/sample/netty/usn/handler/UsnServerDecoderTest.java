aa6908063cd4b7050c137003c99d209df2811048
goddes4 <goddes4@email.com>
2014-04-08 09:01:04 +0000

    바이너리 디코더 테스트 코드 추가

---
+++ test/java/net/octacomm/sample/netty/usn/handler/UsnServerDecoderTest.java
@@ -0,0 +1,69 @@
+package net.octacomm.sample.netty.usn.handler;
+
+
+import static net.octacomm.sample.netty.usn.msg.common.MessageType.*;
+import static org.junit.Assert.*;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.embedded.EmbeddedChannel;
+import net.octacomm.sample.netty.usn.handler.UsnServerDecoder;
+import net.octacomm.sample.netty.usn.handler.UsnServerEncoder;
+import net.octacomm.sample.netty.usn.msg.common.UsnIncomingMessage;
+import net.octacomm.sample.netty.usn.msg.common.UsnMessageHelper;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class UsnServerDecoderTest {
+
+	private EmbeddedChannel ch;
+
+	@Before
+	public void setup() {
+		ch = new EmbeddedChannel(new UsnServerDecoder(), new UsnServerEncoder());
+	}
+	
+	@Test
+	public void testDummyIncoming() {
+		ch.writeInbound(Unpooled.wrappedBuffer(new byte[] {0x02, (byte) 0xFF, 0x01, 23}));
+		
+		UsnIncomingMessage msg = (UsnIncomingMessage)ch.readInbound();
+		assertEquals(msg.getMessageType(), DUMMY_INCOMMING);
+	}
+	
+	@Test
+	public void testDummyOutgoingAck() {
+		ch.writeInbound(Unpooled.wrappedBuffer(new byte[] {(byte) 0x81, (byte) 0xFF, 0x00}));
+		
+		UsnIncomingMessage msg = (UsnIncomingMessage)ch.readInbound();
+		assertEquals(msg.getMessageType(), DUMMY_OUTGOING_ACK);
+	}
+	
+	@Test
+	public void testDummyOutgoing() {
+		ByteBuf expectResult = Unpooled.wrappedBuffer(new byte[] {0x01, 0x00, 0x01, 34});
+		
+		ch.writeOutbound(UsnMessageHelper.makeDummyOutgoing(34));
+		
+		ByteBuf outBuff = (ByteBuf) ch.readOutbound();
+		
+		while (expectResult.isReadable()) {
+			assertEquals(expectResult.readUnsignedByte(), outBuff.readUnsignedByte());
+		}
+	}
+	
+	@Test
+	public void testDummyIncomingAck() {
+		ByteBuf expectResult = Unpooled.wrappedBuffer(new byte[] {(byte) 0x82, 0x00, 0x00});
+		
+		ch.writeOutbound(UsnMessageHelper.makeDummyIncommingAck());
+		
+		ByteBuf outBuff = (ByteBuf) ch.readOutbound();
+		
+		while (expectResult.isReadable()) {
+			assertEquals(expectResult.readUnsignedByte(), outBuff.readUnsignedByte());
+		}
+	}
+	
+}
