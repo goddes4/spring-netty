f5ec06fb8bdc0f81d21c585b08f8e53e41fab6bb
goddes4 <goddes4@email.com>
2014-07-08 02:35:32 +0000

    Netty Handler 로 수신된 메시지를 출력하기 위한 코드 수정 - 기존에는 버퍼를 복사해서 출력했지만, 수신 버퍼를 이용해서 출력하도록 수정

--- src/net/octacomm/util/PrintUtil.java
+++ src/net/octacomm/util/PrintUtil.java
@@ -4,14 +4,12 @@ import io.netty.buffer.ByteBuf;
 
 public class PrintUtil {
 	public static String printReceivedChannelBuffer(String msg, ByteBuf buffer) {
-		ByteBuf copyBuffer = buffer.copy();
 		StringBuilder str = new StringBuilder();
 
 		str.append(msg + " : ");
-		while (copyBuffer.isReadable()) {
-			str.append(String.format("%02X ", copyBuffer.readUnsignedByte()));
+		for (int i = 0; i < buffer.readableBytes(); i++) {
+			str.append(String.format("%02X ", buffer.getUnsignedByte(i)));
 		}
-
 		return str.toString();
 	}
 }
